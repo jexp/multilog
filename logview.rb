eb156cfed2dad949c572adbcb431a3a913b45b55
Michael Hunger <github@jexp.de>
2011-05-12 03:23:48 +0200

    faster and time output on left side

--- logview.rb
+++ logview.rb
@@ -1,7 +1,15 @@
+# usage ruby logview.rb 'Wed May 04 20:16:02 CEST 2011' `find neo4jlogs -name "messages.log"` | less
+# todo show the date just in the leftmost column and remove it from the log output
+
 require 'rubygems'
 require 'chronic'
 
-p ENV['COLUMNS']
+unless ARGV.size > 2
+  puts 'Usage  : ruby logview.rb "date-pattern" logfile1 logfile2 logfile3'
+  puts 'Example: ruby logview.rb "Wed May 04 20:16:02 CEST 2011" `find neo4jlogs -name "messages.log"` | less'
+  exit 
+end
+
 @COLUMNS = 300
 
 @SAMPLE = ARGV.shift
@@ -12,11 +20,6 @@ puts "sample #{@SAMPLE}"
 @count = @names.size
 @WIDTH = @COLUMNS / @count
 
-@files = @names.collect { |name| File.open(name) }
-@lines = @files.collect { |file| file.gets }
-@dates = @names.collect { 0 }
-@min_date = Time.now.to_i
-@file = 0
 
 def datestr(index)
   @lines[index][0..@SAMPLE.size-1]
@@ -27,14 +30,14 @@ def log_date(index)
 end
 
 def valid_date(index)
-  log_date(index) > 0
+  @dates[index] > 0
 end
 
 def next_output
   @min_date = Time.now.to_i
   0.upto(@count-1).each do |i| 
     index = (i+@file) % @count
-    date = log_date(index)
+    date = @dates[index]
     if date < @min_date
       @min_date = date
       @file = index
@@ -43,13 +46,25 @@ def next_output
 end
 
 def print
-  curr_date = log_date(@file)
-  puts 0.upto(@count-1).collect { |i| "%-#{@WIDTH}s |" % (@file==i || log_date(i) == curr_date ? @lines[i] : "")[0..@WIDTH-1] }.join
+  curr_date = @dates[@file]
+  output = 0.upto(@count-1).collect { |i| @dates[i] == curr_date ? @lines[i]||"" : "" }.collect { |s| "%-#{@WIDTH}s |" % s[0..@WIDTH-1] }
+  time = "%-30s |" % (curr_date > 0 ? Time.at(curr_date).to_s : "")
+  puts  time + output.join
+end
+
+def read_line(file)
+  file.gets[0..-2].gsub(/\t/,"  ")
 end
 
 def advance
-  curr_date = log_date(@file)
-  0.upto(@count-1).each { |i| @lines[i] = @files[i].gets[0..-2].gsub(/\t/,"  ")  if @file==i || log_date(i) == curr_date }
+  curr_date = @dates[@file]
+  0.upto(@count-1).each do |i| 
+    if @dates[i] == curr_date
+      @lines[i] = read_line(@files[i])
+      @dates[i] =log_date(i)
+      @lines[i] = @lines[i][@SAMPLE.size..-1] if @dates[i]>0
+    end
+  end
 end
 
 def print_and_advance
@@ -57,6 +72,14 @@ def print_and_advance
   advance
   @lines[@file].nil? || valid_date(@file)
 end
+
+@files = @names.collect { |name| File.open(name) }
+@lines = @names.collect { "" }
+@dates = @names.collect { 0 }
+@min_date = Time.now.to_i
+@file = 0
+
+advance
 
 while true
   next_output
